5adc690934c63812eef7d8549e0ab751e15cb74f
Peter Poon <pope1984@gmail.com>
2015-01-14 01:56:59 -0500

    REV: added debug level for simplerLogger

--- simpleLogger.py
+++ simpleLogger.py
@@ -8,8 +8,13 @@ import os
 import datetime
 import pytz
 
+NOTSET = 0
+DEBUG = 10
+INFO = 20
+ERROR = 30
+
 class SimpleLoggerClass(object):
-    def __init__(self, filename):
+    def __init__(self, filename, logLevel):
         """ determine US Eastern time zone depending on EST or EDT """
         if datetime.datetime.now(pytz.timezone('US/Eastern')).tzname() == 'EDT':
             self.USeasternTimeZone = pytz.timezone('Etc/GMT+4')
@@ -22,20 +27,24 @@ class SimpleLoggerClass(object):
         if not os.path.exists('Log'):
             os.makedirs('Log')
         self._log_file = open('Log/' + filename, 'w')
+        self._logLevel = logLevel # 
         
     def _write_to_log(self, msg):
         currentTime = datetime.datetime.now(tz = self.USeasternTimeZone)
         print msg
         self._log_file.write(str(currentTime) + ": " + msg + '\n')        
+
+    def debug(self, msg):
+        if (self._logLevel <= DEBUG):
+            self._write_to_log(msg)
         
     def info(self, msg):
-        self._write_to_log(msg)
-        
-    def debug(self, msg):
-        self._write_to_log(msg)
+        if (self._logLevel <= INFO):
+            self._write_to_log(msg)
 
     def error(self, msg):
-        self._write_to_log(msg)
+        if (self._logLevel <= ERROR):
+            self._write_to_log(msg)
         
     def close_log(self):
         self._log_file.close()
